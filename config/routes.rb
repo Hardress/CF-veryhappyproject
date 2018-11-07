bb37a1415c908497c32a2a9ec33a941bb875385a
adam pauw <adampauw@gmail.com>
2018-11-07 15:32:46 +0100

    Post: Changed root page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   post 'simple_pages/thank_you'
-  root 'simple_pages#landing_page'
+  # root 'simple_pages#landing_page'
+  root 'products#index'
   post 'payments/create'
   mount ActionCable.server => '/cable'
   
